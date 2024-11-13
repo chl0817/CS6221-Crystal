ea2e4a1019dc1d20587019194fd34cc6e25fcecd
Dania <72161031+dania2512@users.noreply.github.com>
2024-11-13 17:26:21 +0000

    update

--- schema.sql
+++ schema.sql
@@ -1,52 +1,50 @@
-CREATE TABLE Users (
-    user_id SERIAL PRIMARY KEY,
-    username VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL
+CREATE TABLE IF NOT EXISTS Users (
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL
 );
 
-CREATE TABLE ChatRooms (
-    chatroom_id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    event_category VARCHAR(255) NOT NULL,
-    created_by INT NOT NULL,
+CREATE TABLE IF NOT EXISTS ChatRooms (
+    chatroom_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    event_category TEXT NOT NULL,
+    created_by INTEGER NOT NULL,
     FOREIGN KEY (created_by) REFERENCES Users(user_id) ON DELETE CASCADE
 );
 
-CREATE TABLE Messages (
-    message_id SERIAL PRIMARY KEY,
-    chatroom_id INT NOT NULL,
-    user_id INT NOT NULL,
+CREATE TABLE IF NOT EXISTS Messages (
+    message_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    chatroom_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
     message_text TEXT NOT NULL,
     FOREIGN KEY (chatroom_id) REFERENCES ChatRooms(chatroom_id),
     FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
-CREATE TABLE In_ChatRoom (
-    user_id INT NOT NULL,
-    chatroom_id INT NOT NULL,
+CREATE TABLE IF NOT EXISTS In_ChatRoom (
+    user_id INTEGER NOT NULL,
+    chatroom_id INTEGER NOT NULL,
     PRIMARY KEY (user_id, chatroom_id),
     FOREIGN KEY (user_id) REFERENCES Users(user_id),
     FOREIGN KEY (chatroom_id) REFERENCES ChatRooms(chatroom_id)
 );
 
-CREATE TABLE Moderates (
-    user_id INT NOT NULL,
-    chatroom_id INT NOT NULL,
+CREATE TABLE IF NOT EXISTS Moderates (
+    chatroom_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
     PRIMARY KEY (chatroom_id, user_id),
     FOREIGN KEY (chatroom_id) REFERENCES ChatRooms(chatroom_id),
     FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
-CREATE TABLE Banned_From (
-    user_id INT NOT NULL,
-    chatroom_id INT NOT NULL,
-    banned_by INT NOT NULL,
+CREATE TABLE IF NOT EXISTS Banned_From (
+    user_id INTEGER NOT NULL,
+    chatroom_id INTEGER NOT NULL,
+    banned_by INTEGER NOT NULL,
     ban_end TIMESTAMP,
     PRIMARY KEY (user_id, chatroom_id),
     FOREIGN KEY (user_id) REFERENCES Users(user_id),
     FOREIGN KEY (chatroom_id) REFERENCES ChatRooms(chatroom_id),
     FOREIGN KEY (banned_by) REFERENCES Users(user_id)
 );
-
-
