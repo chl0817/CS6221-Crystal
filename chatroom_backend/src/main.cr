4ba390faf266ac3c6730b1d8b4fe613f64c9cbf0
chl302 <chl302@pitt.edu>
2024-11-18 01:26:06 -0500

    backend2

--- chatroom_backend/src/main.cr
+++ chatroom_backend/src/main.cr
@@ -7,38 +7,42 @@ require "./database"
 require "./authentication"
 require "./chat"
 
-config = YAML.parse(File.read("config/config.yml"))
-DB.open SQLite3::Database, config["database"].as_s
+# 读取配置文件
+config = YAML.parse(File.read("config/config.yml")).as_h
 
-get "/" do
-  # Load the main page
-  send_file "public/index.html"
+# 连接 SQLite 数据库
+DB.open "sqlite3://#{config["database"].as_s}" do |db|
+  # 启动 Kemal
+  get "/" do
+    # Load the main page
+    send_file "public/index.html"
+  end
+
+  get "/login" do
+    # Load the login page
+    send_file "public/login.html"
+  end
+
+  get "/register" do
+    # Load the registration page
+    send_file "public/register.html"
+  end
+
+  post "/login" do |env|
+    Authentication.handle_login(env)
+  end
+
+  post "/register" do |env|
+    Authentication.handle_register(env)
+  end
+
+  post "/chat/send" do |env|
+    Chat.handle_message(env)
+  end
+
+  websocket "/chat" do |socket|
+    Chat.handle_websocket(socket)
+  end
+
+  Kemal.run
 end
-
-get "/login" do
-  # Load the login page
-  send_file "public/login.html"
-end
-
-get "/register" do
-  # Load the registration page
-  send_file "public/register.html"
-end
-
-post "/login" do |env|
-  Authentication.handle_login(env)
-end
-
-post "/register" do |env|
-  Authentication.handle_register(env)
-end
-
-post "/chat/send" do |env|
-  Chat.handle_message(env)
-end
-
-websocket "/chat" do |socket|
-  Chat.handle_websocket(socket)
-end
-
-Kemal.run
